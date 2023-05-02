2691388ca5dd370b45197bc4c6fd44d3134c6034
weistapki <weistapki@gmail.com>
2023-05-02 14:55:10 +0200

    solution

--- src/order/Order.java
+++ src/order/Order.java
@@ -1,6 +1,28 @@
 package order;
 
 
-public final class Order {
+public class Order {
+    private final int number;
+    private final String name;
+
+    public Order(int orderNumber, String clientName) {
+        this.number = orderNumber;
+        this.name = clientName;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderNumber=" + number +
+                ", clientName='" + name + '\'' +
+                '}';
+    }
 
 }
